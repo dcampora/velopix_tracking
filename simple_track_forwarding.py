ad31696d6bbddb2ff2553a61be507b53a9d708a9
Daniel Campora <dcampora@cern.ch>
2016-07-01 12:41:49 +0200

    Starting to test things.

--- simple_track_forwarding.py
+++ simple_track_forwarding.py
@@ -2,9 +2,11 @@ from classical_solver import classical_solver
 import event_model as em
 import validator_lite as vl
 import json
+from json import encoder
+encoder.FLOAT_REPR = lambda o: format(o, '.3f')
 
-def open_json(event_number):
-  f = open("velojson/" + str(event_number) + ".json")
+def open_json_event(event_number, container_folder="velojson/"):
+  f = open(container_folder + str(event_number) + ".json")
   json_data = json.loads(f.read())
   event = em.event(json_data)
   f.close()
@@ -18,50 +20,36 @@ def get_normalized_hits(hits, limits):
     n(hits[1], limits, "x"), n(hits[1], limits, "y"), n(hits[1], limits, "z"), \
     n(hits[2], limits, "x"), n(hits[2], limits, "y"), n(hits[2], limits, "z")]
 
-# # Get an event
-# f = open("velojson/1.json")
-# json_data = json.loads(f.read())
-# event = em.event(json_data)
-# f.close()
-
-# # Get all tracks by using the classical method and print them
-# print("Invoking classical solver...")
-# classical = classical_solver()
-# classical_tracks = classical.solve(event)
-# print("Found", len(classical_tracks), "tracks")
-
-# Let's just focus on three-hit tracks, and pass the x, y and z coordinate
-# x, y and z should be in [0, 1]
-# tracks_ghost_list = vl.identify_ghost_tracks(json_data, classical_tracks)
-
-def get_tracks_and_truth(event_number, hit_coord_limits):
-  json_data, event = open_json(event_number)
-  classical = classical_solver()
-  classical_tracks = classical.solve(event)
-  three_hit_tracks = [t for t in classical_tracks if len(t.hits) == 3]
-  if len(three_hit_tracks) > 0:
-    tracks_ghost_list = vl.identify_ghost_tracks(json_data, three_hit_tracks)
-    track_input = [get_normalized_hits(t.hits, hit_coord_limits) for t in three_hit_tracks]
-    truth = tracks_ghost_list
-    return [track_input, truth]
-  else:
+def get_tracks_and_truth(event_number, hit_coord_limits, container_folder="velojson/"):
+  try:
+    json_data, event = open_json_event(event_number, container_folder)
+    classical = classical_solver()
+    classical_tracks = classical.solve(event)
+    three_hit_tracks = [t for t in classical_tracks if len(t.hits) == 3]
+    if len(three_hit_tracks) > 0:
+      tracks_ghost_list = vl.identify_ghost_tracks(json_data, three_hit_tracks)
+      track_input = [get_normalized_hits(t.hits, hit_coord_limits) for t in three_hit_tracks]
+      truth = tracks_ghost_list
+      return [track_input, truth]
+    else:
+      return [[], []]
+  except Exception, e:
+    print(e)
     return [[], []]
 
-def main():
+def main(number_of_events=100, container_folder="velojson/"):
   hit_coord_limits = {
     "x": (-49.702538, 49.741428),
     "y": (-50.506866, 50.526314),
     "z": (-288.031006, 750.531006)
   }
 
-
-  number_of_events = 1
-
   # Let's use several processors to speed this up a little
   from multiprocessing import Pool
   pool = Pool()
 
-  pool_results = [pool.apply_async(get_tracks_and_truth, (i, hit_coord_limits)) for i in range(number_of_events)]
+  pool_results = [pool.apply_async(get_tracks_and_truth, (i, hit_coord_limits, container_folder)) \
+    for i in range(number_of_events)]
   tracks_truth = [res.get(timeout=None) for res in pool_results]
 
   # Join in all results
@@ -74,20 +62,45 @@ def main():
   with open('tracks_truth_data.json', 'w') as outfile:
     json.dump(data, outfile)
 
-  # from neurolab import net
-  # from neurolab import trans
-  # from neurolab import init
-  # from neurolab import train
-  # from neurolab import error
 
-  # sigmoid = trans.LogSig()
-  # ghost_buster = net.newff(minmax=[[0, 1] for _ in range(9)], size=[9, 9, 9], transf=[sigmoid]*3)
-  # for l in ghost_buster.layers:
+def ghost_buster(json_file):
+  f = open(json_file)
+  json_data = json.loads(f.read())
+  f.close()
+
+  from neurolab import net
+  from neurolab import trans
+  from neurolab import init
+  from neurolab import train
+  from neurolab import error
+  import numpy as np
+
+  sigmoid = trans.LogSig()
+  gb_sizes = [9, 9, 9, 9, 1]
+  gb = net.newff(minmax=[[0, 1] for _ in range(9)], size=gb_sizes, transf=[sigmoid]*len(gb_sizes))
+  # for l in gb.layers:
   #   l.initf = init.init_zeros
-  # ghost_buster.init()
+  gb.init()
 
-  # ghost_buster.trainf = train.train_gd
-  # ghost_buster.errorf = error.MSE()
+  gb.trainf = train.train_gd
+  gb.errorf = error.MSE()
+
+  net_input = np.array(json_data["tracks"])
+  net_truth_input = np.array([[a] for a in json_data["truth"]])
+
+  # Train the network
+  sim0 = gb.sim(net_input)
+  error0 = gb.errorf(net_truth_input, sim0)
+  print("Starting error:", error0)
+  gb.train(net_input, net_truth_input, epochs=500, show=20, goal=0.001)
+  sim1 = gb.sim(net_input)
+  error1 = gb.errorf(net_truth_input, sim1)
+  print("Finishing error:", error1)
+  
+  for i in range(len(sim1)):
+    print(training_set["truth"][i], sim1[i])
+
 
 if __name__ == '__main__':
+  # ghost_buster("tracks_truth_data.json")
   main()
