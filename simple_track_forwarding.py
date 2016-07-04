c6b0ef8d5aae0cd91eb1dd63ac0dcf68a108a571
Daniel Campora <dcampora@cern.ch>
2016-07-04 09:00:13 +0200

    Moving to tensorflow.

--- simple_track_forwarding.py
+++ simple_track_forwarding.py
@@ -33,7 +33,7 @@ def get_tracks_and_truth(event_number, hit_coord_limits, container_folder="veloj
       return [track_input, truth]
     else:
       return [[], []]
-  except Exception, e:
+  except e:
     print(e)
     return [[], []]
 
@@ -68,39 +68,108 @@ def ghost_buster(json_file):
   json_data = json.loads(f.read())
   f.close()
 
-  from neurolab import net
-  from neurolab import trans
-  from neurolab import init
-  from neurolab import train
-  from neurolab import error
+  # 275271
+  # print(len(json_data['tracks']))
+
+  import tensorflow as tf
   import numpy as np
 
-  sigmoid = trans.LogSig()
-  gb_sizes = [9, 9, 9, 9, 1]
-  gb = net.newff(minmax=[[0, 1] for _ in range(9)], size=gb_sizes, transf=[sigmoid]*len(gb_sizes))
-  # for l in gb.layers:
-  #   l.initf = init.init_zeros
-  gb.init()
+  # Training set
+  # How about just the relative relation between hits?
+  json_data["slopes"] = []
+  for t in json_data["tracks"]:
+    slopes = [(t[0] - t[3] + 1) / 2, (t[1] - t[4] + 1) / 2, (t[2] - t[5] + 1) / 2,
+      (t[3] - t[6] + 1) / 2, (t[4] - t[7] + 1) / 2, (t[5] - t[8] + 1) / 2]
+    json_data["slopes"].append(slopes)
 
-  gb.trainf = train.train_gd
-  gb.errorf = error.MSE()
+  x = tf.placeholder(tf.float32, [None, 9])
+  W = tf.Variable(tf.zeros([9, 1]))
+  b = tf.Variable(tf.zeros([1]))
 
-  net_input = np.array(json_data["tracks"])
-  net_truth_input = np.array([[a] for a in json_data["truth"]])
-
-  # Train the network
-  sim0 = gb.sim(net_input)
-  error0 = gb.errorf(net_truth_input, sim0)
-  print("Starting error:", error0)
-  gb.train(net_input, net_truth_input, epochs=500, show=20, goal=0.001)
-  sim1 = gb.sim(net_input)
-  error1 = gb.errorf(net_truth_input, sim1)
-  print("Finishing error:", error1)
+  y = tf.nn.softmax(tf.matmul(x, W) + b)
+  y_ = tf.placeholder(tf.float32, [None, 1])
   
-  for i in range(len(sim1)):
-    print(training_set["truth"][i], sim1[i])
+  # cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
+  train_step = tf.train.GradientDescentOptimizer(0.5).minimize(y_ - y)
+  
+  init = tf.initialize_all_variables()
+  sess = tf.Session()
+  sess.run(init)
+
+  # Train the system
+  # for i in range(1000):
+  #   batch_xs = np.array(json_data['tracks'][i*100:((i+1)*100)-1])
+  #   batch_ys = np.array([[a] for a in json_data['truth'][i*100:((i+1)*100)-1]])
+  #   sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+  correct_prediction = tf.equal(y, y_)
+  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  print(correct_prediction, accuracy)
+
+  print("Running predictions")
+  for i in range(1000, 1010):
+    batch_xs = np.array(json_data['tracks'][i*100:((i+1)*100)-1])
+    batch_ys = np.array([[a] for a in json_data['truth'][i*100:((i+1)*100)-1]])
+    print(sess.run(accuracy, feed_dict={x: batch_xs, y_: batch_ys}))
+
+
+  # sigmoid = trans.LogSig()
+  # gb_sizes = [6, 10, 10, 1]
+  # gb = net.newff(minmax=[[0, 1] for _ in range(gb_sizes[0])], size=gb_sizes, transf=[sigmoid]*len(gb_sizes))
+  
+  # net_input = np.array(json_data["slopes"])
+  # net_truth_input = np.array([[a] for a in json_data["truth"]])
+
+  # # Train the network
+  # sim0 = gb.sim(net_input)
+  # error0 = gb.errorf(net_truth_input, sim0)
+  # print("Starting error:", error0)
+  # gb.train(net_input, net_truth_input, epochs=500, show=20, goal=0.001)
+  # sim1 = gb.sim(net_input)
+  # error1 = gb.errorf(net_truth_input, sim1)
+  # print("Finishing error:", error1)
+  
+  # for i in range(len(sim1)):
+  #   print(training_set["truth"][i], sim1[i])
+
+  # ########################
+
+  # import tensorflow as tf
+  # import numpy as np
+
+  # # Create 100 phony x, y data points in NumPy, y = x * 0.1 + 0.3
+  # x_data = np.random.rand(100).astype(np.float32)
+  # y_data = x_data * 0.1 + 0.3
+
+  # # Try to find values for W and b that compute y_data = W * x_data + b
+  # # (We know that W should be 0.1 and b 0.3, but Tensorflow will
+  # # figure that out for us.)
+  # W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
+  # b = tf.Variable(tf.zeros([1]))
+  # y = W * x_data + b
+
+  # # Minimize the mean squared errors.
+  # loss = tf.reduce_mean(tf.square(y - y_data))
+  # optimizer = tf.train.GradientDescentOptimizer(0.5)
+  # train = optimizer.minimize(loss)
+
+  # # Before starting, initialize the variables.  We will 'run' this first.
+  # init = tf.initialize_all_variables()
+
+  # # Launch the graph.
+  # sess = tf.Session()
+  # sess.run(init)
+
+  # # Fit the line.
+  # for step in range(201):
+  #     sess.run(train)
+  #     if step % 20 == 0:
+  #         print(step, sess.run(W), sess.run(b))
+
+  # # Learns best fit is W: [0.1], b: [0.3]
+
 
 
 if __name__ == '__main__':
-  # ghost_buster("tracks_truth_data.json")
-  main()
+  ghost_buster("three_hit_tracks_truth_data.json")
+  # main()
