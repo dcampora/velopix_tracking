a8fd0d9b973d04aacaaa96e2ca9cce882d2742ed
Daniel Campora <dcampora@cern.ch>
2019-04-30 16:40:27 +0200

    Created proper trie.

--- trie_search/search_by_triplet_trie.py
+++ trie_search/search_by_triplet_trie.py
@@ -35,35 +35,30 @@ def calculate_scatter(h0, h1, h2):
 
   return dx * dx + dy * dy
 
-def check_scatter(h0, h1, h2):
-  return calculate_scatter(h0, h1, h2) < max_scatter
-
 def check_compatible_triplets(m0, m1, m2):
   compatible_triplets = []
   for h0 in m0.hits():
     for h1 in m1.hits():
       for h2 in m2.hits():
-        if check_scatter(h0, h1, h2):
-          compatible_triplets.append((h0, h1, h2))
+        scatter = calculate_scatter(h0, h1, h2)
+        if scatter < max_scatter:
+          compatible_triplets.append((h0, h1, h2, scatter))
   return compatible_triplets
 
-def generate_compatible_triplets(module_pairs, consider_missing_layers=False):
-  compatible_triplets_trie = {}
+def generate_compatible_triplets(module_pairs):
+  compatible_triplets_trie = [None] * 26
   for m0, m1 in zip(reversed(module_pairs[2:]), reversed(module_pairs[1:-1])):
     compatible_triplets_module = {}
     compatible_triplets = []
     m2 = m1.module_number - 1
     compatible_triplets += check_compatible_triplets(m0, m1, module_pairs[m2])
-    if m2 - 1 > 0 and consider_missing_layers:
-      compatible_triplets += check_compatible_triplets(m0, module_pairs[m2], module_pairs[m2 - 1])
-      compatible_triplets += check_compatible_triplets(m0, m1, module_pairs[m2 - 1])
     # Iterate compatible_triplets and generate a trie from it
-    for h0, h1, h2 in compatible_triplets:
-      key = (h0, h1)
-      if key not in compatible_triplets_module.keys():
-        compatible_triplets_module[key] = [h2]
-      else:
-        compatible_triplets_module[key].append(h2)
+    for h0, h1, h2, scatter in compatible_triplets:
+      if h0 not in compatible_triplets_module.keys():
+        compatible_triplets_module[h0] = {}
+      if h1 not in compatible_triplets_module[h0].keys():
+        compatible_triplets_module[h0][h1] = []
+      compatible_triplets_module[h0][h1].append((h2, scatter))
     compatible_triplets_trie[m0.module_number] = compatible_triplets_module
   return compatible_triplets_trie
 
@@ -71,7 +66,7 @@ json_data_all_events = []
 all_tracks = []
 
 print("Processing events", end="")
-for event_number in range(9, 10):
+for event_number in range(0, 1):
   print(".", end="")
   sys.stdout.flush()
   # Get an event
@@ -127,12 +122,12 @@ for event_number in range(9, 10):
           forwarding_next_step.append(t)
           t.missed_penultimate_module = t.missed_last_module
           t.missed_last_module = False
-      elif (h0, h1) in compatible_triplets_in_module.keys():
+      elif h0 in compatible_triplets_in_module.keys() and \
+        h1 in compatible_triplets_in_module[h0].keys():
         best_h2 = hit(0, 0, 0, -1)
         best_scatter = max_scatter
-        compatible_h2s = compatible_triplets_in_module[(h0, h1)]
-        for h2 in compatible_h2s:
-          scatter = calculate_scatter(h0, h1, h2)
+        compatible_h2s = compatible_triplets_in_module[h0][h1]
+        for h2, scatter in compatible_h2s:
           if h2 not in flagged_hits and scatter < best_scatter:
             best_h2 = h2
             best_scatter = scatter
@@ -160,18 +155,18 @@ for event_number in range(9, 10):
     forwarding_tracks = forwarding_next_step
 
     # Seeding
-    for h0, h1 in compatible_triplets_in_module.keys():
-      if h0 not in flagged_hits and h1 not in flagged_hits:
-        best_h2 = hit(0, 0, 0, -1)
-        best_scatter = max_scatter
-        compatible_h2s = compatible_triplets_in_module[(h0, h1)]
-        for h2 in compatible_h2s:
-          scatter = calculate_scatter(h0, h1, h2)
-          if h2 not in flagged_hits and scatter < best_scatter:
-            best_h2 = h2
-            best_scatter = scatter
-        if best_h2.id != -1:
-          forwarding_tracks.append(track([h0, h1, best_h2]))
+    for h0 in compatible_triplets_in_module.keys():
+      for h1 in compatible_triplets_in_module[h0].keys():
+        if h0 not in flagged_hits and h1 not in flagged_hits:
+          best_h2 = hit(0, 0, 0, -1)
+          best_scatter = max_scatter
+          compatible_h2s = compatible_triplets_in_module[h0][h1]
+          for h2, scatter in compatible_h2s:
+            if h2 not in flagged_hits and scatter < best_scatter:
+              best_h2 = h2
+              best_scatter = scatter
+          if best_h2.id != -1:
+            forwarding_tracks.append(track([h0, h1, best_h2]))
 
   # Add tracks in forwarding_tracks to either tracks or weak_tracks container
   for t in forwarding_tracks:
